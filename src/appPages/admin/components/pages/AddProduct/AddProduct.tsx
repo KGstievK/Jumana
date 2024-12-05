7857b7b903e181394a3605ad81a58dcef6fe1da6
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-12-05 11:41:56 +0600

    Add Product Photo

--- src/appPages/admin/components/pages/AddProduct/AddProduct.tsx
+++ src/appPages/admin/components/pages/AddProduct/AddProduct.tsx
@@ -1,83 +1,99 @@
+"use client";
 import { FC, useState } from "react";
 import scss from "./AddProduct.module.scss";
-import { remove, edit, close } from "../icons";
+import { close, edit, remove } from "../icons";
 
 interface AddProductProps {
-  _id?: number
-  image: string
+  _id?: number;
+  image: string;
 }
 
 const AddProduct: FC<AddProductProps> = () => {
-  const [create, setCreate] = useState(false)
-	const [edited, setEdited] = useState(false)
-	const [product, setProduct] = useState(null)
+  // const [create, setCreate] = useState(false)
+  // const [edited, setEdited] = useState(false)
+  // const [product, setProduct] = useState(null)
   const [images, setImages] = useState<AddProductProps[]>([]);
-	const [forDeleteImages, setForDeleteImages] = useState<AddProductProps[]>([])
+  // const [forDeleteImages, setForDeleteImages] = useState<AddProductProps[]>([])
 
-	const [imageEditId, setImageEditId] = useState(0)
+  const [imageEditId, setImageEditId] = useState<AddProductProps | number>(0);
 
-
-  
   const addNewImage = (e: any) => {
-    const files = e.target.files[0]
+    const files = e.target.files[0];
     if (files) {
       const object = {
         imageLink: URL.createObjectURL(files),
         image: files,
         id: Date.now(),
-      }
-      setImages([...images, object])
+      };
+      setImages([...images, object]);
     }
-  }
+  };
 
   const deleteImage = (id: number) => {
-    const deletedImage = images.filter((image) => image._id !== id)
-    setImages(deletedImage)
-  }
+    const deletedImage = images.filter((image) => image._id !== id);
+    setImages(deletedImage);
+  };
 
   const editImage = (e: any) => {
-    const files = e.target.files[0]
+    const files = e.target.files[0];
     if (files) {
-      const editedImage = images.map((el) => (el._id === imageEditId ? { ...el, image: files, imageLink: URL.createObjectURL(files) } : el))
-      setImages(editedImage)
-      setImageEditId(0)
+      const editedImage = images.map((el) =>
+        el._id === imageEditId
+          ? { ...el, image: files, imageLink: URL.createObjectURL(files) }
+          : el
+      );
+      setImages(editedImage);
+      setImageEditId(0);
     }
-  }
+  };
 
   return (
     <section className={scss.AddProduct}>
       <div className="container">
         <div className={scss.content}>
-          <div className={scss.Product_image}>
-            <form action="">
-              <h2>Фотографии продукта</h2>
-              <div className={scss.images}>
-              {images.map((el) => (
-							<div key={el._id}>
-								<button
-									onClick={() => {
-										deleteImage(el._id)
-										if (edited) {
-											setForDeleteImages([...forDeleteImages, el._id])
-										}
-									}}
-									className="images__remove">
-									{remove}
-								</button>
-								<label onClick={() => setImageEditId(el._id)} htmlFor="editInput" className="editInput">
-									{edit}
-								</label>
-								<img src={el.image} alt="Image" />
-							</div>
-						))}
-						<input onChange={(e) => editImage(e)} type="file" id="editInput" />
-						<div className="images__block">
-							<label htmlFor="addInput">{close}</label>
-							<input onChange={(e) => addNewImage(e)} type="file" id="addInput" />
+          <form action="">
+            <div className={scss.Product}>
+              <button
+                className={scss.cancel}
+                onClick={() => {
+                  
+                }}
+              >
+                cancel
+              </button>
+              <div className={scss.Product_image}>
+                <h2>Фотографии продукта</h2>
+                <div className={scss.images}>
+                  <div className={scss.images_size}>
+                    <button onClick={() => {}} className={scss.images__remove}>
+                      {remove}
+                    </button>
+                    <label
+                      onClick={() => {}}
+                      htmlFor="editInput"
+                      className={scss.editInput}
+                    >
+                      {edit}
+                    </label>
+                    <img src="" alt="Image" />
+                  </div>
+                  <input
+                    onChange={(e) => editImage(e)}
+                    type="file"
+                    id="editInput"
+                  />
+                  <div className={scss.images__block}>
+                    <label htmlFor="addInput">{close}</label>
+                    <input
+                      onChange={(e) => addNewImage(e)}
+                      type="file"
+                      id="addInput"
+                    />
+                  </div>
+                </div>
               </div>
-
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
     </section>
