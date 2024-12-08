64129eb1fb83db13166608a87590f627fda7ed49
ayat kalybekova <kalybekAyat@outlook.com>
2024-12-08 15:04:36 +0600

    part-1-singlePage

--- src/appPages/site/components/pages/SinglePageSections/SinglePageSection.tsx
+++ src/appPages/site/components/pages/SinglePageSections/SinglePageSection.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import scss from "./SinglePageSection.module.scss";
 import img from "@/assets/images/cardImage.png";
 import star from "@/assets//images//star.png";
+import Link from "next/link";
 
 //! Это Карточка товаров
 
@@ -9,6 +10,10 @@ const SinglePageSection = () => {
   return (
     <section className={scss.SinglePageSection}>
       <div className="container">
+        <div className={scss.header}>
+          <Link href="/">Главная</Link>/<Link href="category">Категории</Link>/
+          <Link href="/">Платья</Link>/ <Link href="">JUMANA “24</Link>
+        </div>
         <div className={scss.content}>
           <div className={scss.images}>
             <Image src={img} alt="photo" />
@@ -32,6 +37,32 @@ const SinglePageSection = () => {
             <div className={scss.price}>
               <del>2700 сом</del>
               <h4>1400 сом</h4>
+            </div>
+
+            <div className={scss.colors}>
+              <h5>Цвета: </h5>
+            </div>
+            <div className={scss.textile}>
+              <h5>Ткань:</h5>
+              <h4>Таффета</h4>
+            </div>
+            <div className={scss.description}>
+              <p>
+                Красивые платья оптом от производителя из Бишкека ,
+                КыргызстанКрасивые платья оптом от производителя из Бишкека ,
+                Кыргызстан
+              </p>
+            </div>
+
+            <div className={scss.sizes}>
+              <h5>Размеры:</h5>
+              <span>XXS</span>
+              <span>XS</span>
+              <span>S</span>
+              <span>M</span>
+              <span>L</span>
+              <span>XL</span>
+              <span>XXL</span>
             </div>
           </div>
         </div>
