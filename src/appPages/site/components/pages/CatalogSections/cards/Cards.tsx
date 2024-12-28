2147b01727508344781f39107eda5bd146a6e5d5
ayat kalybekova <kalybekAyat@outlook.com>
2024-12-28 12:52:18 +0600

    new-page

--- src/appPages/site/components/pages/CatalogSections/cards/Cards.tsx
+++ src/appPages/site/components/pages/CatalogSections/cards/Cards.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import scss from "./cards.module.scss";
-import photo from "@/assets/images/cardImage.png";
 import star from "@/assets/images/star.png";
 import cart from "@/assets/icons/bag-happyBlack.svg";
 import heart from "@/assets/icons/HeartStraight.svg";
@@ -9,93 +8,80 @@ import heartRed from "@/assets/icons/red-heart-icon.svg";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import filterImg from "@/assets/icons/Filter.svg";
+import { useGetAllClothesQuery } from "@/redux/api/category";
+import SideBar from "../sideBar/SideBar";
 
 const Cards = () => {
   const [state, setState] = useState(false);
   const router = useRouter();
-
-  const data = [
-    {
-      title: "JUMANA “24",
-      price: 6000,
-      sale: 20,
-      colors: ["red", "white", "black"],
-      textstyle: "Таффета",
-      description:
-        "Красивые платья оптом от производителя из Бишкека , КыргызстанКрасивые платья оптом от производителя из Бишкека , Кыргызстан",
-      sales: ["xxs", "xs", "s", "m"],
-      starts: 4.95,
-    },
-    {
-      title: "JUMANA “24",
-      price: 6000,
-      sale: 20,
-      colors: ["red", "white", "black"],
-      textstyle: "Таффета",
-      description:
-        "Красивые платья оптом от производителя из Бишкека , КыргызстанКрасивые платья оптом от производителя из Бишкека , Кыргызстан",
-      sales: ["xxs", "xs", "s", "m"],
-      starts: 4.95,
-    },
-    {
-      title: "JUMANA “24",
-      price: 6000,
-      sale: 20,
-      colors: ["red", "white", "black"],
-      textstyle: "Таффета",
-      description:
-        "Красивые платья оптом от производителя из Бишкека , КыргызстанКрасивые платья оптом от производителя из Бишкека , Кыргызстан",
-      sales: ["xxs", "xs", "s", "m"],
-      starts: 4.95,
-    },
-    {
-      title: "JUMANA “24",
-      price: 6000,
-      sale: 20,
-      colors: ["red", "white", "black"],
-      textstyle: "Таффета",
-      description:
-        "Красивые платья оптом от производителя из Бишкека , КыргызстанКрасивые платья оптом от производителя из Бишкека , Кыргызстан",
-      sales: ["xxs", "xs", "s", "m"],
-      starts: 4.95,
-    },
-  ];
+  const { data } = useGetAllClothesQuery();
 
   return (
     <div id={scss.Cards}>
       <div className={scss.content}>
-        <div className={scss.header}>
-          <Image src={filterImg} alt="photo" />
+        <div className={scss.header} onClick={() => <SideBar />}>
+          <Image
+            width={20}
+            height={30}
+            layout="intrinsic"
+            src={filterImg}
+            alt="photo"
+          />
           <h4>ФИЛЬТР</h4>
         </div>
 
         <div className={scss.cards}>
-          {data.map((item, idx) => (
+          {data?.map((item, idx) => (
             <div
               key={idx}
               className={scss.card}
-              onClick={() => router.push("/single")}
+              onClick={() => router.push(`/category/${item.id}`)}
             >
               <div className={scss.blockImg}>
                 <div className={scss.like}>
                   <div className={scss.star}>
-                    <Image src={star} alt="photo" />
-                    <h6>{item.starts}</h6>
+                    <Image
+                      width={500}
+                      height={300}
+                      layout="intrinsic"
+                      src={star}
+                      alt="photo"
+                    />
+                    <h6>{item.average_rating}</h6>
                   </div>
                   <div
                     className={scss.heart}
                     onClick={() => setState((prevState) => !prevState)}
                   >
                     {state ? (
-                      <Image src={heartRed} alt="heart" />
+                      <Image
+                        width={500}
+                        height={300}
+                        layout="intrinsic"
+                        src={heartRed}
+                        alt="heart"
+                      />
                     ) : (
-                      <Image src={heart} alt="heart" />
+                      <Image
+                        width={500}
+                        height={300}
+                        layout="intrinsic"
+                        src={heart}
+                        alt="heart"
+                      />
                     )}
                   </div>
                 </div>
-                <Image src={photo} alt="photo" className={scss.mainImg} />
+                <Image
+                  width={500}
+                  height={300}
+                  layout="intrinsic"
+                  src={item.clothes_photo}
+                  alt="photo"
+                  className={scss.mainImg}
+                />
                 <div className={scss.cart}>
-                  <Image src={cart} alt="cart" />
+                  <Image layout="intrinsic" src={cart} alt="cart" />
                 </div>
               </div>
               <div className={scss.blockText}>
@@ -103,10 +89,11 @@ const Cards = () => {
                   <h4>Product Category</h4>
                   <div className={scss.colors}>❤️</div>
                 </div>
-                <h2>JUMANA “22</h2>
+                <h2>{item.clothes_name}</h2>
                 <div className={scss.price}>
-                  <span>{item.price - (item.price * item.sale) / 100}com</span>
-                  <del>{item.price}c</del>
+                  {/* <span>{item.price - (item.price * item.sale) / 100}com</span> */}
+                  {/* <del>{item.price}c</del> */}
+                  <span>{item.price}</span>
                 </div>
               </div>
             </div>
