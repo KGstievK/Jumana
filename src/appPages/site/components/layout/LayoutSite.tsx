898906b6a0ff8458824fce56365806c70d7432da
ayat kalybekova <kalybekAyat@outlook.com>
2025-01-22 13:25:58 +0600

    just started tabBar

--- src/appPages/site/components/layout/LayoutSite.tsx
+++ src/appPages/site/components/layout/LayoutSite.tsx
@@ -1,23 +1,22 @@
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer/Footer'
-import Header from './Header/Header'
-import scss from './LayoutSite.module.scss'
-import { FC, ReactNode } from 'react'
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import scss from "./LayoutSite.module.scss";
+import { FC, ReactNode } from "react";
+import Tabbar from "../ui/TabBar/Tabbar";
 
 interface LayoutSiteProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
-const LayoutSite:FC<LayoutSiteProps> = ({children}) => {
+const LayoutSite: FC<LayoutSiteProps> = ({ children }) => {
   return (
     <div className={scss.LayoutSite}>
-      <Header/>
-      <main>
-        {children}
-      </main>
-      <Footer/>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+      <Tabbar />
     </div>
-  )
-}
+  );
+};
 
-export default LayoutSite
+export default LayoutSite;
